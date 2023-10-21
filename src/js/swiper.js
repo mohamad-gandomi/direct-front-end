169cab988de4908ca7b20110ff3d167d6bd2ac56
gandomi <m.gandomi1995@gmail.com>
2023-10-21 11:12:06 +0330

    add ticker section

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,2 +1,19 @@
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
+
+//======================================================================
+// TICKER CAROUSEL
+//======================================================================
+
+const TICKER = new Swiper('.ticker__carousel', {
+    loop: true,
+    spaceBetween: 20,
+    preventClicks: true,
+    autoplay: {
+        delay: 5,
+        disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    speed: 6000,
+    grabCursor: true,
+});
