4a7c1c30c8e33e714793a8f9fd6d45bcf70717ac
gandomi <m.gandomi1995@gmail.com>
2023-10-22 12:35:39 +0330

    add customer testimonials

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -17,3 +17,64 @@ const TICKER = new Swiper('.ticker__carousel', {
     speed: 6000,
     grabCursor: true,
 });
+
+//======================================================================
+// VIDEO CAROUSEL
+//======================================================================
+
+const videoCarousel = new Swiper('.video-carousel', {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    preventClicks: true,
+    speed: 800,
+    autoplay: {
+        delay: 3000,
+    },
+    breakpoints: {
+        1200: {
+            slidesPerView: 4,
+        },
+        // when window width is >= 768px
+        768: {
+            slidesPerView: 3,
+        },
+    }
+});
+
+//======================================================================
+// CUSTOMER LOGO CAROUSEL
+//======================================================================
+
+const customerLogoCarousel = new Swiper('.customer-logo-carousel', {
+    slidesPerView: "auto",
+    spaceBetween: 20,
+    preventClicks: true,
+    speed: 800,
+    autoplay: {
+        delay: 1500,
+    },
+});
+
+//======================================================================
+// CUSTOMER LOGO CAROUSEL
+//======================================================================
+
+const customerTestimonialsCarousel = new Swiper('.customer-testimonials-carousel', {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    preventClicks: true,
+    speed: 800,
+    autoplay: {
+        delay: 3000,
+    },
+    breakpoints: {
+        1300: {
+            slidesPerView: 4,
+        },
+        // when window width is >= 768px
+        768: {
+            slidesPerView: 2,
+        },
+    }
+});
+
