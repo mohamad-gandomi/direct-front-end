e55d4c27134db46b0ed2b98f310721d48d109e78
gandomi <m.gandomi1995@gmail.com>
2023-10-24 16:25:51 +0330

    add light to main page

--- src/js/custom.js
+++ src/js/custom.js
@@ -2,28 +2,30 @@
 //
 //  Table of Contents
 //
-//    1. Close Other Tabs When One Tab Opens
+//    1. Prouct Quantity For Product Page
 //
 //---------------------------------------------
 
 
 //---------------------------------------------
-// 1. Close Other Tabs When One Tab Opens
+// 1. Prouct Quantity For Product Page
 //---------------------------------------------
 document.addEventListener("DOMContentLoaded", function () {
     const quantityInput = document.getElementById("quantity");
     const minusButton = document.querySelector(".quantity-button.minus");
     const plusButton = document.querySelector(".quantity-button.plus");
 
-    minusButton.addEventListener("click", function () {
-        let currentValue = parseInt(quantityInput.value) || 0;
-        if (currentValue > 1) {
-            quantityInput.value = currentValue - 1;
-        }
-    });
+    if (quantityInput && minusButton && plusButton) {
+        minusButton.addEventListener("click", function () {
+            let currentValue = parseInt(quantityInput.value) || 0;
+            if (currentValue > 1) {
+                quantityInput.value = currentValue - 1;
+            }
+        });
 
-    plusButton.addEventListener("click", function () {
-        let currentValue = parseInt(quantityInput.value) || 0;
-        quantityInput.value = currentValue + 1;
-    });
+        plusButton.addEventListener("click", function () {
+            let currentValue = parseInt(quantityInput.value) || 0;
+            quantityInput.value = currentValue + 1;
+        });
+    }
 });
